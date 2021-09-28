043e44f42497c58f2d40dfd9e5ec7303362e4b8a
Sisterx840 <omrggs@gmail.com>
2021-09-28 19:32:50 +0900

    [upload] Frame 파일 수정

--- Frame.java
+++ Frame.java
@@ -1,26 +1,61 @@
+package TaxiVAT;
+
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JButton;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+
 public class Frame {
+
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		JLabel label = new JLabel("Some text");
-		JButton btn1 = new JButton("Click me!");
+		JButton btn1 = new JButton("Clikc me!!");
+		JButton btn2 = new JButton("Exit");
 		JTextArea txtArea = new JTextArea();
 		JTextField txtField = new JTextField(200);
+		JPanel btnPanel = new JPanel();
 		panel.setLayout(new BorderLayout());
 		
+		
+		
+		
+		panel.setLayout(new BorderLayout());
+		
+
+		btnPanel.add(btn1);
+		btnPanel.add(btn2);
 		panel.add(label, BorderLayout.NORTH);
-		panel.add(btn1, BorderLayout.WEST);
+		panel.add(btnPanel, BorderLayout.WEST);
 		panel.add(txtArea, BorderLayout.CENTER);
+		
+		btn1.addActionListener(new ActionListener() {		
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					label.setText(txtArea.getText());
+			}
+
+					
+	});
+	
+		btn2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+			
 		
 		frame.add(panel);
 		
@@ -30,5 +65,7 @@ public class Frame {
 		frame.setSize(840, 840/12*9);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
+
 }
